f65d3ddc57c0e2dbef608be07ed0e53886ff7ca4
Edward Leonardo Pico Cabra <epicocabra@gmail.com>
2021-04-20 14:45:37 -0500

    programación finalizada

---
+++ src/config/global.js
@@ -0,0 +1,251 @@
+export const global = {
+  componenteFormativo: 'Acciones de mejora y su seguimiento',
+  descripcionCurso:
+    'El programa de extensionismo tecnológico del SENA busca que los profesionales consultores que trabajan la metodología desde los roles de gestor consultor y extensionista tecnológico adquieran las competencias propicias y desarrolladas en el marco de la implementación de la estrategia y, al mismo tiempo, fortalezcan sus saberes, conocimientos y competencias relacionados con la gestión empresarial y gestión informática requerida para el cambio de la transformación digital en las empresas en Colombia. </br> Para este componente se abordarán las etapas del plan de mejora en la cual se definirán las variables sobre las que se implementará el plan de mejora, que en su totalidad incluirá, el autodiagnóstico previamente desarrollado, las necesidades identificadas, la línea base, la proyección de mejora sobre esta línea base, las actividades a desarrollar, las formaciones/capacitaciones sugeridas, el tiempo de implementación proyectado y las tecnologías planteadas para dar cumplimiento a los objetivos a alcanzar con las empresas.',
+  imagenBannerPrincipal: require('@/assets/curso/banner-princiapal.svg'),
+  fondoBannerPrincipal: require('@/assets/curso/fondo-banner-principal.png'),
+}
+
+export const menuPrincipal = {
+  menu: [
+    {
+      nombreRuta: 'inicio',
+      icono: 'fas fa-home',
+      titulo: 'Volver al inicio',
+    },
+    {
+      nombreRuta: 'tema1',
+      icono: 'far fa-file-alt',
+      numero: '1',
+      titulo: 'Acciones de mejora y su seguimiento',
+      desarrolloContenidos: true,
+      subMenu: [
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.1',
+          titulo:
+            'Correlación entre interpretación de plan de recomendaciones de plataforma SoftWhere, necesidades empresario y plan de mejora',
+          hash: 't_1_1',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.2',
+          titulo: 'Actividades del plan de mejora',
+          hash: 't_1_2',
+        },
+        {
+          icono: 'far fa-file-alt',
+          numero: '1.3',
+          titulo:
+            'Acompañamiento e Implementación de actividades y formato de seguimiento',
+          hash: 't_1_3',
+        },
+      ],
+    },
+  ],
+  subMenu: [
+    // {
+    //   nombreRuta: 'actividad',
+    //   icono: 'far fa-question-circle',
+    //   titulo: 'Actividad didáctica',
+    //   desarrolloContenidos: true,
+    // },
+    {
+      nombreRuta: 'glosario',
+      icono: 'fas fa-sort-alpha-down',
+      titulo: 'Glosario',
+    },
+    {
+      nombreRuta: 'complementario',
+      icono: 'far fa-folder-open',
+      titulo: 'Material complementario',
+    },
+    {
+      icono: 'fas fa-book',
+      titulo: 'Referencias bibliográficas',
+      nombreRuta: 'referencias',
+    },
+    {
+      icono: 'fas fa-download',
+      titulo: 'Descargar material',
+      download: 'downloads/material.zip',
+    },
+    {
+      icono: 'far fa-registered',
+      titulo: 'Créditos',
+      nombreRuta: 'creditos',
+    },
+  ],
+}
+
+export const creditos = {
+  liderEquipo: {
+    nombre: 'Maria Camila Garcia Santamaria',
+    cargo: 'Líder del equipo',
+  },
+  contenidoInstruccional: [
+    {
+      nombre: 'Rafael Neftalí Lizcano Reyes',
+      cargo: 'Asesor metodológico y pedagógico',
+      centro: 'Centro Industrial del Diseño y la Manufactura',
+      regional: 'Regional Santander',
+    },
+    {
+      nombre: 'Luis Guillermo Alvarez García',
+      cargo: 'Experta temática',
+      centro: 'Centro De Servicios y Gestión Empresarial',
+      regional: 'Regional Antioquia',
+    },
+    {
+      nombre: 'Alejandro Moncada Betancur',
+      cargo: 'Experta temática',
+      centro: 'Centro De Procesos Industriales y Construcción',
+      regional: 'Regional Caldas',
+    },
+    {
+      nombre: 'Leidy Yohana Ortiz Diaz',
+      cargo: 'Experto temático',
+      centro: 'Centro de la Industria, la Empresa y los Servicios',
+      regional: 'Regional Norte de Santander',
+    },
+    {
+      nombre: 'Gimena Santacruz Benítez',
+      cargo: 'Experto temático',
+      centro: 'Centro de la Industria, la Empresa y los Servicios Regional,',
+      regional: 'Regional Huila',
+    },
+    {
+      nombre: 'Sergio Arturo Medina Castillo',
+      cargo: 'Diseñador instruccional',
+      centro: 'Centro para la industria de la Comunicación Gráfica',
+      regional: 'Regional Distrito Capital',
+    },
+    {
+      nombre: 'José Gabriel Ortiz Abella',
+      cargo: 'Evaluador instruccional',
+      centro: 'Centro para la Industria de la Comunicación Gráfica',
+      regional: 'Regional Distrito Capital',
+    },
+    {
+      nombre: 'María Isabel Román Rueda',
+      cargo: 'Revisora de estilo',
+      centro: 'Centro Industrial Del Diseño Y La Manufactura',
+      regional: 'Regional Santander',
+    },
+  ],
+  desarrolloProducto: [
+    {
+      nombre: 'Francisco José Lizcano Reyes',
+      cargo: 'Responsable del equipo',
+    },
+    {
+      nombre: 'Leyson Fabian Castaño Perez',
+      cargo: 'Integración de recursos y pruebas',
+    },
+    {
+      nombre: [
+        'Camilo Andrés Villamizar Lizcano',
+        'Eulises Orduz Amezquita',
+        'José Jaime Luis Tang Pinzón',
+        'Lady Adriana Ariza Luque',
+        'Oleg Litvin',
+        'Victor Raúl Cárdenas Cáceres',
+        'Zuleidy Maria Ruiz Torres',
+      ],
+      cargo: 'Diseño web y Producción Audiovisual',
+    },
+    {
+      nombre: 'Edward Leonardo Pico Cabra',
+      cargo: 'Desarrollo Front-End',
+    },
+    {
+      centro: 'Centro Industrial del Diseño y la Manufactura',
+      regional: 'Regional Santander',
+    },
+  ],
+  gestoresRepositorio: [
+    {
+      nombre: 'Milady Tatiana Villamil Castellanos',
+      cargo: 'Validación de recursos',
+      centro: 'Centro de comercio y servicios',
+      regional: 'Regional Tolima',
+    },
+  ],
+}
+
+export const referencias = [
+  {
+    referencia:
+      'Colombia Productiva. (2019). Diagnóstico de madurez tecnológica de las empresas. Ministerio de Industria y Comercio de Colombia. SoftWhere.',
+  },
+  {
+    referencia:
+      'DNP. (2012). Productividad.  Programa de Extensión Tecnológica para Colombia. DNP',
+  },
+  {
+    referencia:
+      'DNP, (2012). Rentabilidad. Programa de Extensión Tecnológica para Colombia. DNP',
+  },
+  {
+    referencia:
+      'Organización Internacional de Normalización. (2015). Proceso. ',
+    link: 'https://www.iso.org/obp/ui/es/#iso:std:iso:9000:ed-3:v1:es',
+  },
+  {
+    referencia: 'Raffino, M. (2020). Áreas funcionales de una empresa.',
+    link: 'https://concepto.de/areas-funcionales-de-una-empresa/',
+  },
+  {
+    referencia: 'Roldán, P. (s. f). Tecnología.',
+    link:
+      'https://economipedia.com/definiciones/tecnologia.html#:~:text=La%20tecnolog%C3%ADa%20es%20el%20conjunto,mejorar%20su%20calidad%20de%20vida',
+  },
+]
+
+export const glosario = [
+  {
+    termino: 'Áreas funcionales',
+    significado:
+      'Las áreas funcionales de la empresa son las diversas actividades más importantes de la misma, ya que por ellas se plantean y tratan de alcanzar los objetivos y metas. Generalmente una empresa está formada mínimo por cinco (5) áreas funcionales básicas (dirección, administración, ventas, producción y contabilidad y finanzas), pero puede estar formada por muchas más (investigación, recursos humanos, estrategia, etc.). El número de áreas funcionales en las pequeñas empresas se simplifican y se integran unas dentro de las otras, esto ayuda a tener un mejor desempeño dentro de la empresa.  ●	Área de dirección: área imprescindible y considerada la cabeza de la empresa. Lleva el timón de la empresa, establece los objetivos y la dirige hacia ellos. Está relacionada con el resto de áreas funcionales, ya que es la que controla todas las áreas de trabajo de la empresa.    ●	Área de administración: está relacionada con el funcionamiento de la empresa. Es la operación de negocio en sentido general, desde contrataciones y pagos a personal. Por lo general es el emprendedor o empresario quien se encarga de esta área funcional. Está relacionada con otras áreas como recursos humanos.  ●	Área de ventas: orientada al exterior. Aquí se plantean las estrategias que la empresa seguirá en el área del marketing, los mercados donde la empresa opera, los segmentos de mercado, el ciclo de vida de los productos, diseño de nuevos productos para la ganancia de dinero.  ●	Área de producción: aquí se lleva a cabo la producción de los bienes que la empresa comercializará después en los centros, mercados, tiendas de venta. ●	Área de contabilidad y finanzas: reglamentario para todas las empresas, pues es obligatorio que lleven un registro contable. Tendrá en cuenta todos los movimientos de dinero, tanto dentro como fuera de la empresa, que también en algunas veces pueden estar almacenadas en bancos o en una caja fuerte.',
+  },
+  {
+    termino: 'Proceso',
+    significado:
+      'Es una secuencia de pasos con algún tipo de lógica que se enfoca en lograr algún resultado específico, el cual tiene unas entradas, una transformación y unos resultados. ',
+  },
+  {
+    termino: 'Productividad',
+    significado:
+      'Es la relación entre la cantidad de productos obtenida por un sistema productivo y los recursos utilizados para obtener dicha producción.',
+  },
+  {
+    termino: 'Rentabilidad',
+    significado:
+      'Permite comparar el resultado que se ha obtenido con el desarrollo de la actividad de la empresa con las inversiones que hemos realizado para obtener dicho resultado. Obtenemos un resultado al que todavía no hemos restado los intereses, gastos ni impuestos. Se conoce comúnmente como EBITDA.',
+  },
+  {
+    termino: 'Tecnología',
+    significado:
+      'Conjunto de conocimientos y técnicas que se aplican de manera ordenada para alcanzar un determinado objetivo o resolver un problema.',
+  },
+]
+
+export const complementario = [
+  {
+    texto: 'Colombia Productiva - Fedesoft - Softwhere',
+    tipo: 'Sitio web',
+    link: 'https://www.softwhere.com.co ',
+  },
+  {
+    texto:
+      'Ministerio del Interior, Ministerio Hacienda y Crédito, MAcademia de extensionismo tecnológico, (25 de septiembre de 2020). Parte 1. Semana de transferencia de conocimientos nuevos colaboradores. YouTube.',
+    tipo: 'Video',
+    link: 'https://www.youtube.com/watch?v=shhELBfknBs&t=1361s',
+  },
+  {
+    texto:
+      'Academia de extensionismo tecnológico, (25 de septiembre de 2020). Parte 2. Semana de transferencia de conocimientos nuevos colaboradores. YouTube.',
+    tipo: 'Video',
+    link: 'https://www.youtube.com/watch?v=Q1NywhRPbqQ&t=875s',
+  },
+]
---
+++ src/views/curso/Tema1.vue
@@ -0,0 +1,244 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3 1
+      .h3 Acciones de mejora y su seguimiento
+    .titulo-segundo#t_1_1.mt-5
+      .h4 1.1  Correlación entre interpretación de plan de recomendaciones de plataforma SoftWhere, necesidades empresario y plan de mejora
+
+    .row.justify-content-center.align-items-center.mt-5
+      .col-12.col-md-8
+        p Tomando como partida el autodiagnóstico y las necesidades previamente validadas, se procede a revisar con el empresario el plan de mejora propuesto y sus variables, estableciendo la actual línea base o punto de partida.
+
+        p.my-3 Igualmente, se corrobora con el empresario el nivel de madurez tecnológica en las dos (2) dimensiones a través de la validación (pregunta-respuesta, observación, revisión de información) y se verifica que la empresa se encuentre en un proceso de usabilidad tecnológica (existencia de tecnología, adaptación y uso estratégico de la misma en los procesos internos de la compañía) según su nivel de madurez e igualmente con las 15 variables y sus perspectivas. 
+
+        p Adicionalmente, se debe incluir en esta etapa la definición de las áreas específicas en las cuales se realizará el acompañamiento por parte del programa de extensionismo y cuáles serían las capacitaciones certificadas en MinTIC y SENA que las empresas realizarán con base en su nivel de madurez. 
+
+
+      .col-12.col-md-4
+        .imagen-titulo--izq
+          figure.my-3
+            img(src='@/assets/ilustraciones/img1.png', alt='Texto que describa la imagen')      
+
+    .row.mt-5
+      .col-12
+        .cajon--azul.p-4.mb-5.mb-lg-0
+          p Técnicas como la lluvia o tormenta de ideas, o el grupo nominal, entre otras, buscan facilitar el análisis de las problemáticas definidas de manera estructurada, permitiendo definir actividades clave en pro de la mejora organizacional. Se busca, entonces, revisar las debilidades transversales de la organización, presentadas en el diagnóstico y definir acciones clave para sobrellevarlas. Estas acciones se definen como una lista de actividades para ser realizadas cumpliendo el objetivo clave de la consultoría.
+    
+    p Dentro del análisis e interpretación se plantea que el autodiagnóstico de SoftWhere se enmarca en seis (6) áreas funcionales, las cuales permiten definir un análisis y unas actividades clave que deben ser priorizadas en el plan.
+    .hr-dotted.mt-5    
+    
+    .row.justify-content-center.align-items-center.mt-5
+      .col-12.col-md-6 
+        p Para consultar más información acerca del autodiagnóstico, acceder al siguiente material complementario de libre acceso:   
+      .col-12.col-md-6  
+        a.anexo.mt-3(:href="('https://drive.google.com/drive/folders/170o8xUZkVRH2v8aYx20v646T6CfmBLlg')" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-pdf.svg")
+          .anexo__texto
+            p Anexo. Términos de referencia - Diccionario Práctico de Extensionismo Tecnológico. 2020  
+    .h6.text-center.my-5 Áreas funcionales  
+    .row.justify-content-center.align-items-center.p-5.bg-1
+      .col-12   
+        img.img-t.img-a(src='@/assets/ilustraciones/img2.png', alt='') 
+
+        AcordionA.mt-5(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Operaciones")
+
+            p Son aquellas actividades que tienen relación directa con las diferentes áreas de la organización que generan un producto o servicio que se ofrece a los clientes. Enmarcan la forma de hacer las cosas en la empresa para lograr tener un producto o servicio que se ofrece al cliente final. 
+
+            p.mt-3 SoftWhere plantea, frente a las operaciones, los ajustes definidos para lograr la alineación de los componentes del modelo de negocio, soportado en sistemas de información definidos para la gestión de los procesos operacionales de la empresa, permitiendo contar con la capacidad para manejar los volúmenes de información requeridos, con la granularidad y calidad requerida o con la necesaria integración de los sistemas que soportan las áreas del negocio.
+
+          div(titulo="Clientes")
+            p Es la persona u organización que compra servicios o productos a la empresa. Además, es el principal enfoque de atención de las organizaciones ya que enmarcan el mercado objetivo sobre el que se define el foco de atención de la empresa. Sobre el cliente se determina el plan o estrategias de marketing de la organización.
+
+            p.mt-3 El autodiagnóstico determina el grado de implementación y uso de herramientas tecnológicas que permitan analizar los aspectos más representativos y claves de los clientes y el mercado en general. Aquí se busca revisar cuáles son los aspectos para fortalecer en el modelo de negocio de la empresa y que generen oportunidades de acceso a nuevos mercados, productos y clientes, y defendiendo su posición frente a sus competidores.
+
+          div(titulo="Gestión")
+            p La correcta gestión depende, principalmente, de la toma de decisiones de manera correcta y oportuna. El desarrollo tecnológico evidencia la necesidad de las empresas en innovar constantemente, definiendo las metodológicas y técnicas necesarias sobre las que deben enmarcar sus objetivos y, por ende, su modelo de negocio. En este contexto cobra especial relevancia la gestión empresarial de los directivos que define el conjunto de medidas y estrategias que buscan mejorar la competitividad y productividad de la empresa.
+
+            p.mt-3 El diagnóstico determina los ajustes para lograr la alineación e integración sistémica de los procesos del negocio, a través de estrategias y sistemas de información que permitan la correcta gestión de las áreas del negocio con la granularidad y calidad requeridas.
+
+          div(titulo="Gestión tecnológica")
+            p Es el conjunto de conocimientos, actividades y herramientas que brindan valor a las organizaciones a través de procesos de transformación digital efectivos y que permitan facilitar la ejecución de tareas que generen mayor competitividad en el entorno.
+
+            p.mt-3 El mercado actual ha permitido una apertura hacia la apropiación tecnológica de las organizaciones, por ello se plantean mecanismos que generan un cambio en la forma de hacer los negocios, una transición a nivel productivo y mejoras sostenibles en los modelos de negocio. Lo anterior enmarca una modernización de las empresas hacia la generación de nuevas estrategias para generar mayor competitividad. 
+
+            p.mt-3 En el entorno actual, la gestión tecnológica se extiende de manera transversal a los procesos y áreas de la empresa, partiendo desde la base de gestión de conocimiento de los miembros de la organización y la digitalización de procesos generales de la estructura de negocio.
+
+            p.mt-3 La gestión tecnológica plantea desde el diagnóstico la debilidad en sistemas de información para una correcta gestión de datos, así como la baja integración de la información que permita la toma de decisiones basada en conocimiento del negocio.
+
+          div(titulo="Estrategia")
+            p es el puente entre las políticas y los objetivos definidos para el cumplimiento de las propuestas de valor enmarcadas en los modelos de negocio.
+
+            p.mt-3 Entonces, se define que la estrategia es el marco para tomar decisiones en el mercado y la estructura operativa del negocio. Las estrategias definen desde las inversiones hacia las prioridades operativas y marketing hasta contrataciones y enfoques de ventas, entre otros aspectos.
+
+            p.mt-3 Frente al diagnóstico, la tecnología se convierte en un activo empresarial estratégico y un aliado para los objetivos comerciales de la organización. La estrategia plantea la gestión efectiva de la información de los clientes que permita conocer sus expectativas, mejorar su satisfacción y, por tanto, asegurar la fidelización y recompra de los productos o servicios.
+
+          div(titulo="Financiera")
+            p el área financiera se enmarca en el proceso económico de la organización, determina la viabilidad económica de los modelos de negocio corporativos y su optimización frente a las diferentes variaciones del mercado.
+
+            p.mt-3 La organización plantea desde este concepto, un proceso de entendimiento sobre la rentabilidad y los beneficios que trae consigo el uso de soluciones tecnológicas en la organización y su influencia en la estructura financiera de la empresa.
+
+    .titulo-segundo#t_1_2.mt-5
+      .h4 1.2 Actividades del plan de mejora 
+    .row.justify-content-center.align-items-center.mt-5
+      .col-12.col-md-8
+        p Las actividades presentadas en el plan de mejora son planteadas de acuerdo con las necesidades definidas en el diagnóstico realizado. Estas necesidades determinan una ruta a desarrollar para el logro de los objetivos propuestos en el plan de mejora.
+
+        p.mt-3 Dependiendo del plan de mejora se valida si la empresa se encuentra en un proceso de usabilidad tecnológica (existencia de tecnología, adaptación y uso estratégico de la misma en los procesos internos de la compañía) de acuerdo con su nivel de madurez digital y la evaluación de las áreas funcionales.
+
+        p.mt-3 Es así, como las actividades presentadas en el plan de mejora están orientadas sobre áreas específicas a las cuales se les debe realizar acompañamiento por parte del programa de extensionismo. Además, se deben definir las capacitaciones certificadas en MinTIC y SENA que las empresas realizarán según su nivel de madurez y el plan de mejora definido.
+      .col-12.col-md-4
+        .imagen-titulo--izq
+          figure.my-3
+            img(src='@/assets/ilustraciones/img3.png', alt='Texto que describa la imagen') 
+          .imagen-titulo__titulo
+            .h4.m-0.text-white Plan de mejora   
+
+    .row.mt-5
+      .col-12
+        .cajon--rojo.p-4.mb-5.mb-lg-0
+          p Las actividades definen estratégicamente el foco (área, proceso, personas) y tiempos de trabajo enmarcados en las variables del autodiagnóstico, tales como estrategia, gestión, directivos, relación con clientes, servicios postventa, clientes, operaciones, logística externa, financiera, estructura de costos, flujo de ingresos, compras, gestión de tecnología, colaboradores y sociedad civil.  
+
+    .titulo-segundo#t_1_3.mt-5
+      .h4 1.3 Acompañamiento e Implementación de actividades y formato de seguimiento 
+    p.mt-5 El proceso de acompañamiento en el marco de la consultoría tiene como propósito la supervisión de las actividades planteadas en el plan de mejora, definiendo un marco de control clave en la implementación organizacional.           
+    .imagen-titulo--izq.mt-5
+      figure
+        img(src='@/assets/ilustraciones/img4.png', alt='Texto que describa la imagen')
+    p.mt-5 Este proceso se propone para evitar retrasos en tiempos y temáticas que pueden desarticular el plan propuesto inicialmente.
+
+    p.mt-3 Frente a la estrategia se define realizar un seguimiento cronológico del desarrollo de las actividades.
+
+    p.mt-3 El consultor debe dedicar tiempo para supervisar el estado de cada una de las actividades que está desarrollando, prestando especial interés a aquellas que están sufriendo algún retraso.  En el momento en que se detecta cualquier desviación o demora en el proceso hay que analizar las causas para poder efectuar las correcciones oportunas y recuperar el tiempo perdido en el acompañamiento empresarial. 
+
+
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import BannerInterno from '../../components/plantilla/BannerInterno'
+export default {
+  name: 'Tema1',
+  components: {
+    BannerInterno,
+  },
+  data: () => ({
+    datosLineaTiempoA: [
+      {
+        ano: '100.000 - 10.000 A.C.',
+        titulo: 'Período Paleolítico',
+        texto: 'Pieles, pelo de animales, hojas, huesos y conchas.',
+      },
+      {
+        ano: '10.000 - 5.000 A.C.',
+        titulo: 'Periodo Mesolítico',
+        texto:
+          'Broches hechos con hueso, abrigos, gorros, botas y zapatillas de cuero.',
+      },
+      {
+        ano: '10.000 - 5.000 A.C.',
+        titulo: 'Periodo Neolítico',
+        texto: 'Vestimenta de fibras animales y vegetales, pulseras de marfil.',
+      },
+    ],
+    datosLineaTiempoB: [
+      {
+        titulo: '01 de marzo',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus augue, pulvinar ac auctor at, pellentesque id diam. Praesent maximus, felis sollicitudin pharetra vestibulum, turpis tortor tincidunt augue, in efficitur urna diam eleifend magna.',
+        icono: require('@/assets/componentes/ej-05.svg'),
+      },
+      {
+        titulo: '02 de marzo',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus augue, pulvinar ac auctor at, pellentesque id diam. Praesent maximus, felis sollicitudin pharetra vestibulum, turpis tortor tincidunt augue, in efficitur urna diam eleifend magna.',
+        icono: require('@/assets/componentes/ej-05.svg'),
+      },
+      {
+        titulo: '03 de marzo',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus augue, pulvinar ac auctor at, pellentesque id diam. Praesent maximus, felis sollicitudin pharetra vestibulum, turpis tortor tincidunt augue, in efficitur urna diam eleifend magna.',
+        icono: require('@/assets/componentes/ej-05.svg'),
+      },
+      {
+        titulo: '04 de marzo',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus augue, pulvinar ac auctor at, pellentesque id diam. Praesent maximus, felis sollicitudin pharetra vestibulum, turpis tortor tincidunt augue, in efficitur urna diam eleifend magna.',
+        icono: require('@/assets/componentes/ej-05.svg'),
+      },
+    ],
+    datosPasosA: [
+      {
+        titulo: 'Dressed for Error',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.',
+      },
+      {
+        titulo: 'Before the Fury',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.',
+      },
+      {
+        titulo: 'Khans Tribute',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.',
+      },
+      {
+        titulo: 'Secret of the Quiet Stranger',
+        texto:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor gravida aliquam. Morbi orci urna, iaculis in ligula et, posuere interdum lectus.',
+      },
+    ],
+    datosSlyder: [
+      {
+        titulo:
+          'Never, never assume that what you have achieved is good enough',
+        texto:
+          'Your rapidograph pens are dried up, the x-acto blades in your bag are rusty, and your mind is dull. Stop clicking your mouse, get messy, go back to the basics and make something original.',
+        imagen: require('@/assets/template/img-placeholder.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo:
+          'Never, never assume that what you have achieved is good enough',
+        texto:
+          'Your rapidograph pens are dried up, the x-acto blades in your bag are rusty, and your mind is dull. Stop clicking your mouse, get messy, go back to the basics and make something original.',
+        imagen: require('@/assets/template/img-placeholder.svg'),
+        // leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo:
+          'Never, never assume that what you have achieved is good enough',
+        texto:
+          'Your rapidograph pens are dried up, the x-acto blades in your bag are rusty, and your mind is dull. Stop clicking your mouse, get messy, go back to the basics and make something original.',
+        imagen: require('@/assets/template/img-placeholder.svg'),
+        leyendaImagen: 'Leyenda de la imagen',
+      },
+      {
+        titulo:
+          'Never, never assume that what you have achieved is good enough',
+        texto:
+          'Your rapidograph pens are dried up, the x-acto blades in your bag are rusty, and your mind is dull. Stop clicking your mouse, get messy, go back to the basics and make something original.',
+        imagen: require('@/assets/template/img-placeholder.svg'),
+        leyendaImagen: 'Leyenda de la imagen',
+      },
+    ],
+  }),
+  mounted() {
+    this.increment()
+    setTimeout(() => {
+      this.decrease()
+    }, 3000)
+  },
+  methods: {
+    ...mapActions(['increment', 'decrease']),
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
